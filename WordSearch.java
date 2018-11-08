20dea511f9e33aaf75865f8177b0574afd5c3a28
Sublime <other@Alberts-MBP.fios-router.home>
2018-11-07 20:37:05 -0500

    fix errors

--- WordSearch.java
+++ WordSearch.java
@@ -1,11 +1,6 @@
 public class WordSearch{
     private char[][]data;
 
-    /**Initialize the grid to the size specified
-     *and fill all of the positions with '_'
-     *@param row is the starting height of the WordSearch
-     *@param col is the starting width of the WordSearch
-     */
     public WordSearch(int rows,int cols){
       data = new char[rows][cols];
       for (int x = 0; x < rows; x++){
@@ -15,7 +10,6 @@ public class WordSearch{
       }
     }
 
-    /**Set all values in the WordSearch to underscores'_'*/
     private void clear(){
       for (int x = 0; x < data.length; x++){
         for (int y = 0; y < data[x].length;y++){
@@ -24,10 +18,6 @@ public class WordSearch{
       }
     }
 
-    /**Each row is a new line, there is a space between each letter
-     *@return a String with each character separated by spaces, and rows
-     *separated by newlines.
-     */
     public String toString(){
       String var = "";
       for (int x = 0; x < data.length; x++){
@@ -39,25 +29,13 @@ public class WordSearch{
       return var;
     }
 
-
-    /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from left to right, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical location of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     * or there are overlapping letters that do not match, then false is returned
-     * and the board is NOT modified.
-     */
     public boolean addWordHorizontal(String word,int row, int col){
       int check = col + word.length() - 1;
       if (check >= data[row].length){
         return false;
       }
       for (int x =0; x < word.length();x++){
-        if (word.charAt(x) == data[row][col] || word.charAt(x) == '_'){
+        if (word.charAt(x) == data[row][col] || data[row][col] == '_'){
           data[row][col] = word.charAt(x);
         }else{
           return false;
@@ -84,7 +62,7 @@ public class WordSearch{
         return false;
       }
       for (int x =0; x < word.length();x++){
-        if (word.charAt(x) == data[row][col] || word.charAt(x) == '_'){
+        if (word.charAt(x) == data[row][col] || data[row][col] == '_'){
           data[row][col] = word.charAt(x);
         }else{
           return false;
